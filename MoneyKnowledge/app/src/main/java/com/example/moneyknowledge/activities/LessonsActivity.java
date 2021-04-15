b8431ed079e7e10dd8e2c895559a37df20b0fbf3
TanaseNicoleta <nicoleta.tanase@hotmail.com>
2021-04-15 23:58:11 +0300

    Added drawer menu + please fix that annoying burger menu icon

--- MoneyKnowledge/app/src/main/java/com/example/moneyknowledge/activities/LessonsActivity.java
+++ MoneyKnowledge/app/src/main/java/com/example/moneyknowledge/activities/LessonsActivity.java
@@ -1,16 +1,22 @@
 package com.example.moneyknowledge.activities;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
-import android.view.View;
-import android.widget.ImageView;
+import android.view.MenuItem;
 
 import com.example.moneyknowledge.R;
+import com.google.android.material.navigation.NavigationView;
 
-public class LessonsActivity extends AppCompatActivity {
-
-    ImageView menu;
+public class LessonsActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+    DrawerLayout drawerLayout;
+    NavigationView navView;
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,15 +24,37 @@ public class LessonsActivity extends AppCompatActivity {
         setContentView(R.layout.activity_lessons);
         initComponents();
 
-        menu.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
 
-            }
-        });
     }
 
     private void initComponents() {
-        menu = findViewById(R.id.menu);
+       drawerLayout = findViewById(R.id.drawer_layout);
+       navView = findViewById(R.id.nav_view);
+       toolbar = findViewById(R.id.toolbar);
+       setSupportActionBar(toolbar);
+       toolbar.setLogo(R.drawable.ic_baseline_menu_24);
+
+       //Navigation drawer menu
+        navView.bringToFront();
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this,drawerLayout, toolbar, R.string.open_nav, R.string.close_nav);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
+        navView.setNavigationItemSelectedListener(this);
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        if(drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        return true;
     }
 }

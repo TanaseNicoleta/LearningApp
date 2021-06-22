8b40e47304b35b76f2a469b54c65398b3cde708c
TanaseNicoleta <nicoleta.tanase@hotmail.com>
2021-06-22 23:51:56 +0300

    Tests Done +1

--- MoneyKnowledge/app/src/main/java/com/example/moneyknowledge/activities/TestActivity.java
+++ MoneyKnowledge/app/src/main/java/com/example/moneyknowledge/activities/TestActivity.java
@@ -1,31 +1,19 @@
 package com.example.moneyknowledge.activities;
 
 import androidx.annotation.NonNull;
-import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.moneyknowledge.R;
-import com.example.moneyknowledge.activities.register.RegisterActivity;
-import com.example.moneyknowledge.adapter.LessonsListAdapter;
-import com.example.moneyknowledge.adapter.QuestionListAdapter;
 import com.example.moneyknowledge.model.Answers;
-import com.example.moneyknowledge.model.Lesson;
 import com.example.moneyknowledge.model.LessonProgress;
 import com.example.moneyknowledge.model.Question;
-import com.example.moneyknowledge.model.User;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
@@ -35,12 +23,9 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
 import java.text.SimpleDateFormat;
-import java.util.ArrayList;
-import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
 import java.util.Locale;
-import java.util.Random;
 
 public class TestActivity extends AppCompatActivity {
     public static final String LESSON_ID = "lessonId";

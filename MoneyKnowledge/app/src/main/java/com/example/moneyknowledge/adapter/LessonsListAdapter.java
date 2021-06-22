5fd5a2679f6f3dee247598a02d46db9c330da918
TanaseNicoleta <nicoleta.tanase@hotmail.com>
2021-06-23 00:55:39 +0300

    Set seekbar value

--- MoneyKnowledge/app/src/main/java/com/example/moneyknowledge/adapter/LessonsListAdapter.java
+++ MoneyKnowledge/app/src/main/java/com/example/moneyknowledge/adapter/LessonsListAdapter.java
@@ -5,6 +5,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.SeekBar;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -12,14 +13,27 @@ import androidx.annotation.Nullable;
 
 import com.example.moneyknowledge.R;
 import com.example.moneyknowledge.model.Lesson;
+import com.example.moneyknowledge.model.LessonProgress;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.List;
 
 public class LessonsListAdapter extends ArrayAdapter<Lesson> {
+    public static final String NOTE = "note";
     private Context context;
     private int resource;
     private List<Lesson> lessons;
     private LayoutInflater inflater;
+
+    final DatabaseReference database = FirebaseDatabase.getInstance().getReference(NOTE);
+    final FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+    final String userId = user.getUid();
 
     public LessonsListAdapter(@NonNull Context context, int resource, @NonNull List<Lesson> objects, LayoutInflater inflater) {
         super(context, resource, objects);
@@ -34,18 +48,31 @@ public class LessonsListAdapter extends ArrayAdapter<Lesson> {
     public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
         View view = inflater.inflate(resource, parent, false);
         Lesson lesson = lessons.get(position);
+        TextView textView = view.findViewById(R.id.lesson_title);
+        populateTV(lesson.getTitle(), textView);
+        SeekBar sb = view.findViewById(R.id.setProgressSB);
 
-        //Seteaza si progresul automat din firebase
-        if(lesson != null) {
-            addLessonName(view, lesson.getTitle());
-        }
+        database.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                for (DataSnapshot dataSnapshot : snapshot.getChildren()) {
+                    LessonProgress grade = dataSnapshot.getValue(LessonProgress.class);
+                    if(grade.getId_user().equals(userId) && grade.getId_lesson().equals(lesson.getId()))
+                        sb.setProgress(grade.getProgress());
+                    else
+                        sb.setProgress(0);
+
+                }
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
 
         return view;
-    }
-
-    private void addLessonName(View view, String name) {
-        TextView textView = view.findViewById(R.id.lesson_title);
-        populateTV(name, textView);
     }
 
     private void populateTV(String name, TextView textView) {

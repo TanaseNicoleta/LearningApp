a6eba371ce32a41d9b93b66f1e8894376c22875a
TanaseNicoleta <nicoleta.tanase@hotmail.com>
2021-07-14 00:57:23 +0300

    O incercare de barchart

--- MoneyKnowledge/app/src/main/java/com/example/moneyknowledge/activities/ReportsActivity.java
+++ MoneyKnowledge/app/src/main/java/com/example/moneyknowledge/activities/ReportsActivity.java
@@ -8,29 +8,105 @@ import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.MenuItem;
 
 import com.example.moneyknowledge.R;
+import com.example.moneyknowledge.model.LessonProgress;
 import com.example.moneyknowledge.model.Question;
+import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
+import com.github.mikephil.charting.utils.ColorTemplate;
 import com.google.android.material.navigation.NavigationView;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
 
 public class ReportsActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+    public static final String NOTE = "note";
     DrawerLayout drawerLayout;
     NavigationView navView;
     Toolbar toolbar;
+
+    final DatabaseReference database = FirebaseDatabase.getInstance().getReference(NOTE);
+    final FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+    final String userId = user.getUid();
+    int i = 0;
+
+    BarChart barChart;
+    BarData barData;
+    BarDataSet barDataSet;
+    ArrayList barEntries;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_reports);
         initMenuComponents();
+        initComponents();
+    }
 
 
+    public void initComponents() {
+        barChart = findViewById(R.id.barChart);
+        SetData();
 
     }
 
+    private void SetData() {
+        barEntries = new ArrayList();
+
+        barEntries.add(new BarEntry(Float.parseFloat(Integer.toString(2)), 0));
+        barEntries.add(new BarEntry(4f, 1));
+        barEntries.add(new BarEntry(3f, 2));
+        barEntries.add(new BarEntry(0f, 3));
+        barEntries.add(new BarEntry(1f, 4));
+        barEntries.add(new BarEntry(9f, 5));
+        barEntries.add(new BarEntry(5f, 6));
+
+        barDataSet = new BarDataSet(barEntries, "");
+        barData = new BarData(barDataSet);
+        barChart.setData(barData);
+        barDataSet.setColors(ColorTemplate.JOYFUL_COLORS);
+        barDataSet.setValueTextColor(Color.WHITE);
+        barDataSet.setValueTextSize(12f);
+
+//        database.addValueEventListener(new ValueEventListener() {
+//            @Override
+//            public void onDataChange(@NonNull DataSnapshot snapshot) {
+//                for (DataSnapshot dataSnapshot : snapshot.getChildren()) {
+//                    LessonProgress grade = dataSnapshot.getValue(LessonProgress.class);
+//                    if(grade.getId_user().equals(userId) && grade.getGrade() > 0) {
+//                        barEntries.add(new BarEntry(Float.parseFloat(Integer.toString(grade.getGrade())), i));
+//                        i++;
+//                    }
+//
+//                }
+//                barDataSet = new BarDataSet(barEntries, "");
+//                barData = new BarData(barDataSet);
+//                barChart.setData(barData);
+//                barDataSet.setColors(ColorTemplate.JOYFUL_COLORS);
+//                barDataSet.setValueTextColor(Color.WHITE);
+//                barDataSet.setValueTextSize(12f);
+//            }
+//
+//            @Override
+//            public void onCancelled(@NonNull DatabaseError error) {
+//
+//            }
+//        });
+
+
+    }
 
     //Drawer Menu
     private void initMenuComponents() {

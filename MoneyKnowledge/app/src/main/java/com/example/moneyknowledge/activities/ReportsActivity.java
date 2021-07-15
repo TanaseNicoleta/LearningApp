7c2830491a7df3bbf9c2a8565e0beca9fd242c3a
TanaseNicoleta <nicoleta.tanase@hotmail.com>
2021-07-16 00:24:47 +0300

    Better graphs

--- MoneyKnowledge/app/src/main/java/com/example/moneyknowledge/activities/ReportsActivity.java
+++ MoneyKnowledge/app/src/main/java/com/example/moneyknowledge/activities/ReportsActivity.java
@@ -14,16 +14,24 @@ import android.util.Log;
 import android.view.MenuItem;
 
 import com.example.moneyknowledge.R;
+import com.example.moneyknowledge.model.Lesson;
 import com.example.moneyknowledge.model.LessonProgress;
 import com.example.moneyknowledge.model.Question;
+import com.example.moneyknowledge.utils.CategoryBarChartXaxisFormatter;
 import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.charts.HorizontalBarChart;
 import com.github.mikephil.charting.charts.PieChart;
+import com.github.mikephil.charting.components.Legend;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.components.YAxis;
 import com.github.mikephil.charting.data.BarData;
 import com.github.mikephil.charting.data.BarDataSet;
 import com.github.mikephil.charting.data.BarEntry;
 import com.github.mikephil.charting.data.PieData;
 import com.github.mikephil.charting.data.PieDataSet;
 import com.github.mikephil.charting.data.PieEntry;
+import com.github.mikephil.charting.formatter.IndexAxisValueFormatter;
+import com.github.mikephil.charting.interfaces.datasets.IBarDataSet;
 import com.github.mikephil.charting.utils.ColorTemplate;
 import com.google.android.material.navigation.NavigationView;
 import com.google.firebase.auth.FirebaseAuth;
@@ -35,6 +43,7 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class ReportsActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
     public static final String NOTE = "note";
@@ -47,10 +56,12 @@ public class ReportsActivity extends AppCompatActivity implements NavigationView
     final String userId = user.getUid();
     int i = 0;
 
-    BarChart barChart;
+    HorizontalBarChart barChart;
     BarData barData;
     BarDataSet barDataSet;
-    ArrayList barEntries;
+    ArrayList<BarEntry> barEntries = new ArrayList<>();
+    ArrayList<String> labels = new ArrayList<>();
+
 
     PieChart pieChart;
     PieData pieData;
@@ -67,27 +78,52 @@ public class ReportsActivity extends AppCompatActivity implements NavigationView
         SetData();
         dataValues();
 
+        labels.add("economie-1");
+        labels.add("economie-2");
+        labels.add("economie-3");
+        labels.add("economie-4");
+        labels.add("economie-5");
+
     }
 
     private void SetData() {
         database.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
-                barEntries = new ArrayList();
                 for (DataSnapshot dataSnapshot : snapshot.getChildren()) {
                     LessonProgress grade = dataSnapshot.getValue(LessonProgress.class);
-                    barEntries.add(new BarEntry(i, Float.parseFloat(Integer.toString(grade.getGrade()))));
+                    barEntries.add(new BarEntry(i, Float.parseFloat(Integer.toString(grade.getGrade())), grade.getId_lesson()));
                     i++;
-
                 }
                 Log.i("mesaj", barEntries.toString());
-                barDataSet = new BarDataSet(barEntries, "Note");
+                List<IBarDataSet> dataSets = new ArrayList<>();
+                barDataSet = new BarDataSet(barEntries, "");
                 barData = new BarData(barDataSet);
+                String[] xAxisLabels = labels.toArray(new String[0]);
+
+                barDataSet.setStackLabels(xAxisLabels);
+                barChart.getXAxis().setValueFormatter(new IndexAxisValueFormatter(labels));
+
                 barChart.setData(barData);
-                barDataSet.setColors(ColorTemplate.JOYFUL_COLORS);
+                dataSets.add(barDataSet);
+                barData.addDataSet(barDataSet);
+                barData.setDrawValues(true);
+                barData.setBarWidth(0.4f);
+                barChart.getAxisLeft().setAxisMaximum(10);
+                barChart.getAxisLeft().setAxisMinimum(1);
+
+                barChart.setFitBars(true);
+                barChart.setData(barData);
+
+                barChart.setDrawValueAboveBar(false);
+                barChart.setDescription(null);
+                barDataSet.setColors(ColorTemplate.LIBERTY_COLORS);
                 barDataSet.setValueTextColor(Color.WHITE);
                 barDataSet.setValueTextSize(12f);
                 barChart.setPinchZoom(true);
+                barChart.getLegend().setEnabled(false);
+
+                barChart.invalidate();
             }
             @Override
             public void onCancelled(@NonNull DatabaseError error) {
@@ -109,9 +145,12 @@ public class ReportsActivity extends AppCompatActivity implements NavigationView
                 }
                 Log.i("mesaj2", pieEntries.toString());
                 pieDataSet = new PieDataSet(pieEntries, "");
-                pieDataSet.setColors(ColorTemplate.COLORFUL_COLORS);
-                PieData pieData = new PieData(pieDataSet);
+                pieDataSet.setColors(ColorTemplate.MATERIAL_COLORS);
+                pieDataSet.setValueTextSize(12f);
+                pieData = new PieData(pieDataSet);
                 pieChart.setData(pieData);
+                pieChart.getLegend().setEnabled(false);
+                pieChart.setDescription(null);
                 pieChart.invalidate();
             }
             @Override

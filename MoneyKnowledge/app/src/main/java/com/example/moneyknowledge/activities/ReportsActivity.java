5bb6bfde4ee98b46bb07eed55b1bc264c134a311
TanaseNicoleta <nicoleta.tanase@hotmail.com>
2021-07-17 17:58:07 +0300

    Rapoarte

--- MoneyKnowledge/app/src/main/java/com/example/moneyknowledge/activities/ReportsActivity.java
+++ MoneyKnowledge/app/src/main/java/com/example/moneyknowledge/activities/ReportsActivity.java
@@ -17,15 +17,20 @@ import com.example.moneyknowledge.R;
 import com.example.moneyknowledge.model.Lesson;
 import com.example.moneyknowledge.model.LessonProgress;
 import com.example.moneyknowledge.model.Question;
+import com.github.mikephil.charting.charts.BarChart;
 import com.github.mikephil.charting.charts.HorizontalBarChart;
 import com.github.mikephil.charting.charts.PieChart;
+import com.github.mikephil.charting.components.AxisBase;
+import com.github.mikephil.charting.components.XAxis;
 import com.github.mikephil.charting.data.BarData;
 import com.github.mikephil.charting.data.BarDataSet;
 import com.github.mikephil.charting.data.BarEntry;
 import com.github.mikephil.charting.data.PieData;
 import com.github.mikephil.charting.data.PieDataSet;
 import com.github.mikephil.charting.data.PieEntry;
+import com.github.mikephil.charting.formatter.IAxisValueFormatter;
 import com.github.mikephil.charting.formatter.IndexAxisValueFormatter;
+import com.github.mikephil.charting.formatter.ValueFormatter;
 import com.github.mikephil.charting.interfaces.datasets.IBarDataSet;
 import com.github.mikephil.charting.utils.ColorTemplate;
 import com.google.android.material.navigation.NavigationView;
@@ -50,13 +55,19 @@ public class ReportsActivity extends AppCompatActivity implements NavigationView
     final FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
     final String userId = user.getUid();
     int i = 0;
+    int sumEc = 0;
+    int sumFin = 0;
+    int sumAcc = 0;
 
-    HorizontalBarChart barChart;
+    BarChart barChart;
     BarData barData;
     BarDataSet barDataSet;
     ArrayList<BarEntry> barEntries = new ArrayList<>();
     ArrayList<String> labels = new ArrayList<>();
 
+   ArrayList<Integer> economyGrades = new ArrayList<>();
+   ArrayList<Integer> financeGrades = new ArrayList<>();
+   ArrayList<Integer> accountigGrades = new ArrayList<>();
 
     PieChart pieChart;
     PieData pieData;
@@ -79,42 +90,25 @@ public class ReportsActivity extends AppCompatActivity implements NavigationView
         database.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
+                barEntries = new ArrayList();
                 for (DataSnapshot dataSnapshot : snapshot.getChildren()) {
                     LessonProgress grade = dataSnapshot.getValue(LessonProgress.class);
-                    barEntries.add(new BarEntry(i, Float.parseFloat(Integer.toString(grade.getGrade())), grade.getId_lesson()));
-                    labels.add(grade.getId_lesson());
-                    i++;
+                    if(grade.getId_user().equals(userId) && grade.getGrade() > 0) {
+                        barEntries.add(new BarEntry(i, Float.parseFloat(Integer.toString(grade.getGrade()))));
+                        i++;
+                    }
                 }
-                Log.i("mesaj", barEntries.toString());
-                List<IBarDataSet> dataSets = new ArrayList<>();
+
                 barDataSet = new BarDataSet(barEntries, "");
-                barData = new BarData(barDataSet);
-                String[] xAxisLabels = labels.toArray(new String[0]);
-
-                barDataSet.setStackLabels(xAxisLabels);
-                barChart.getXAxis().setValueFormatter(new IndexAxisValueFormatter(labels));
-
-                barChart.setData(barData);
-                dataSets.add(barDataSet);
-                barData.addDataSet(barDataSet);
-                barData.setDrawValues(true);
-                barData.setBarWidth(0.4f);
-                barChart.getAxisLeft().setAxisMaximum(10);
-                barChart.getAxisLeft().setAxisMinimum(1);
-
-                barChart.setFitBars(true);
-                barChart.setData(barData);
-
-                barChart.setDrawValueAboveBar(false);
-                barChart.setDescription(null);
-                barDataSet.setColors(ColorTemplate.LIBERTY_COLORS);
-                barDataSet.setValueTextColor(Color.WHITE);
+                barDataSet.setColors(ColorTemplate.MATERIAL_COLORS);
                 barDataSet.setValueTextSize(12f);
-                barChart.setPinchZoom(true);
+                barData = new BarData(barDataSet);
+                barChart.setData(barData);
                 barChart.getLegend().setEnabled(false);
-
-                barChart.invalidate();
+                barChart.setDescription(null);
+                pieChart.invalidate();
             }
+
             @Override
             public void onCancelled(@NonNull DatabaseError error) {
 
@@ -129,11 +123,38 @@ public class ReportsActivity extends AppCompatActivity implements NavigationView
                 pieEntries = new ArrayList();
                 for (DataSnapshot dataSnapshot : snapshot.getChildren()) {
                     LessonProgress grade = dataSnapshot.getValue(LessonProgress.class);
-                    if(grade.getId_user().equals(userId) && grade.getGrade() > 0) {
-                        pieEntries.add(new PieEntry(Float.parseFloat(Integer.toString(grade.getGrade())), grade.getId_lesson()));
+                    if(grade.getId_lesson().contains("economie") && grade.getGrade() > 0) {
+                        economyGrades.add(grade.getGrade());
+                    }
+                    if(grade.getId_lesson().contains("finante") && grade.getGrade() > 0) {
+                        financeGrades.add(grade.getGrade());
+                    }
+                    if(grade.getId_lesson().contains("contabilitate") && grade.getGrade() > 0) {
+                        accountigGrades.add(grade.getGrade());
                     }
                 }
-                Log.i("mesaj2", pieEntries.toString());
+                Log.i("mesaj2", accountigGrades.toString());
+                Log.i("mesaj21", financeGrades.toString());
+                Log.i("mesaj22", economyGrades.toString());
+
+                for (int i=0 ; i<economyGrades.size(); i++) {
+                    sumEc+=economyGrades.get(i);
+                }
+                for (int i=0 ; i<financeGrades.size(); i++) {
+                    sumFin+=financeGrades.get(i);
+                }
+                for (int i=0 ; i<accountigGrades.size(); i++) {
+                    sumAcc+=accountigGrades.get(i);
+                }
+
+                if (economyGrades.size()>0)
+                    pieEntries.add(new PieEntry(sumEc/economyGrades.size(), "Economie"));
+                if (financeGrades.size()>0)
+                    pieEntries.add(new PieEntry(sumFin/financeGrades.size(), "Finante"));
+                if (accountigGrades.size()>0)
+                    pieEntries.add(new PieEntry(sumAcc/accountigGrades.size(), "Contabilitate"));
+
+
                 pieDataSet = new PieDataSet(pieEntries, "");
                 pieDataSet.setColors(ColorTemplate.MATERIAL_COLORS);
                 pieDataSet.setValueTextSize(12f);
